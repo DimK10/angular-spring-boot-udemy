524feb7e22e0992dfff80ea77e672b2f369c3222
dkaitantzidis <dkaitantzidis@byte.gr>
2023-09-01 16:28:59 +0300

    Lesson 58 finished

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Product } from '../common/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient
+      .get<GetResponse>(this.baseUrl)
+      .pipe(map((response: GetResponse) => response._embedded.products));
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  };
 }
