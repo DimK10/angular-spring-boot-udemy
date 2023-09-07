536f1ecfa895ff2af36adbb3b530c80e2103aff3
dkaitantzidis <dkaitantzidis@byte.gr>
2023-09-07 14:45:58 +0300

    Lesson 109 finished

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const routes: Routes = [
   { path: 'products/:id', component: ProductDetailsComponent },
@@ -37,6 +38,7 @@ const routes: Routes = [
     HttpClientModule,
     NgOptimizedImage,
     RouterModule.forRoot(routes),
+    NgbModule,
   ],
   providers: [ProductService],
   bootstrap: [AppComponent],
