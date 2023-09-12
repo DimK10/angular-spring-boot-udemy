44857acc01a85c9f40fef5bbb9bd2079b9713a77
dkaitantzidis <dkaitantzidis@byte.gr>
2023-09-12 16:02:12 +0300

    Lesson 153 finished

--- angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -18,6 +18,37 @@ export class CheckoutComponent implements OnInit {
         lastName: [''],
         email: [''],
       }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: [''],
+      }),
     });
+  }
+
+  onSubmit() {
+    console.log('Handling form submission');
+    console.log(this.checkoutFormGroup.get('customer').value);
+    console.log(
+      'The email address is: ' +
+        this.checkoutFormGroup.get('customer').value.email,
+    );
   }
 }
