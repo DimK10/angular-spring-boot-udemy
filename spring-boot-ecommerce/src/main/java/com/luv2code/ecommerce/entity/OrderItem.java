49fc0b63212be3db7546239562900f92516572d5
dkaitantzidis <dkaitantzidis@byte.gr>
2023-10-17 10:15:22 +0300

    Lesson 202 finished

---
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/OrderItem.java
@@ -0,0 +1,26 @@
+package com.luv2code.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_item")
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "product_id")
+    private Long productId;
+}
