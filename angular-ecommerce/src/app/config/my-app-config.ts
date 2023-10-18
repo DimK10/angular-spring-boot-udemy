1c2fdbf27fc4f90137e121ab13c3bea74639ada5
dkaitantzidis <dkaitantzidis@byte.gr>
2023-10-18 10:35:17 +0300

    Lesson 226 finished

---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oacto0tancLuMIwi5d7',
+    issuer: 'https://dev-60977729.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
